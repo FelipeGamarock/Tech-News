a99bb1099853a8fa19021b4cb539c536009aee2f
Felipe Gama Danta <epilefgama2@gmail.com>
2022-11-14 17:26:27 -0300

    feat: req 8, search by tag

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -6,8 +6,8 @@ from datetime import datetime
 def search_by_title(title):
     query = {"title": {"$regex": title, "$options": "i"}}
     news_found = search_news(query)
-    title_and_urls = [(new['title'], new['url'])for new in news_found]
-    return title_and_urls
+    titles_and_urls = [(new['title'], new['url'])for new in news_found]
+    return titles_and_urls
 
 
 # Requisito 7
@@ -16,15 +16,18 @@ def search_by_date(date):
         formated_data = datetime.fromisoformat(date).strftime("%d/%m/%Y")
         query = {"timestamp": formated_data}
         news_found = search_news(query)
-        title_and_urls = [(new['title'], new['url'])for new in news_found]
-        return title_and_urls
+        titles_and_urls = [(new['title'], new['url'])for new in news_found]
+        return titles_and_urls
     except ValueError:
         raise ValueError('Data inválida')
 
 
 # Requisito 8
 def search_by_tag(tag):
-    """Seu código deve vir aqui"""
+    query = {"tags": {"$regex": tag, "$options": "i"}}
+    news_found = search_news(query)
+    titles_and_urls = [(new['title'], new['url'])for new in news_found]
+    return titles_and_urls
 
 
 # Requisito 9
